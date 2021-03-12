1805a43368b0671bdd8cb4e6d4461b7ebdc82ee5
jenkins <jenkins@localhost.localdomain>
2021-03-12 15:56:17 +0530

    jenkins

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,7 +5,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/jenkins/prod /tmp/.auth sanjeev@192.168.0.199:/tmp/.auth
-scp -i /home/jenkins/prod ./jenkins/deploy/publish sanjeev@192.168.0.199:/tmp/publish
+scp -i /opt/prod /tmp/.auth sanjeev@192.168.0.199:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish sanjeev@192.168.0.199:/tmp/publish
 
-ssh -i /home/jenkins/prod sanjeev@192.168.0.199 "/tmp/publish"
+ssh -i /opt/prod sanjeev@192.168.0.199 "/tmp/publish"
